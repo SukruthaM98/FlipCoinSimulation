526a60e447ac5924d0d29fdb92d73d8db92117f8
SukruthaM98 <msukrutha@gmail.com>
2020-10-06 19:43:09 +0530

    computed number of times head won and number of times tail won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -11,3 +11,23 @@ then
 else
 	echo " coin flips tail"
 fi
+
+#number of heads and tails
+countofhead=0
+countoftail=0
+
+for (( i=0; i<20; i++ ))
+do
+	CoinFlip=$(( RANDOM%2 ))
+
+	if [ $CoinFlip -eq 0 ]
+	then
+        	echo " coin flips head"
+		countofhead=$(($countofhead+1))
+	else
+        	echo " coin flips tail"
+		countoftail=$(($countoftail+1))
+	fi
+done
+echo " number of times head won" $countofhead
+echo "number of times tail won" $countoftail
